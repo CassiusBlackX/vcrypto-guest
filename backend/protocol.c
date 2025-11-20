839a311c89d63161f220fbed0dd8aebbeaa1f50e
CassiusBlackX <cassiusx@qq.com>
2025-11-20 08:37:26 +0000

    rename param be_connfd to client_fd

--- backend/protocol.c
+++ backend/protocol.c
@@ -12,37 +12,30 @@
 #include "sess.h"
 #include "../frontend/aes_cbc.h"
 
-extern cdev_resource* cr;
-
-extern struct rte_ring *shared_ring;
-extern struct rte_mempool* sym_crypto_session_pool;
-extern struct rte_mempool* sym_crypto_op_mempool;
-extern struct rte_mempool* pktmbuf_mempool;
-
-bool vcrypto_be_protocol_engine_init(int be_connfd) {
-  if (be_connfd < 0) {
+bool vcrypto_be_protocol_engine_init(int cliend_fd) {
+  if (cliend_fd < 0) {
     log_error("invalid be_connfd");
     return false;
   }
 
   bool ret = true;
-  ret &= vcrypto_send(be_connfd, cr->shared_ring->name, sizeof(cr->shared_ring->name));
-  ret &= vcrypto_send(be_connfd, &be_connfd, sizeof(be_connfd));
+  ret &= vcrypto_send(cliend_fd, cr->shared_ring->name, sizeof(cr->shared_ring->name));
+  ret &= vcrypto_send(cliend_fd, &cliend_fd, sizeof(cliend_fd));
   return ret;
 }
 
-bool vcrypto_be_protocol_create_sess(int be_connfd) {
+bool vcrypto_be_protocol_create_sess(int client_fd) {
   cipher_auth_ctrl cipher_auth;
   bool ret = true;
-  ret &= vcrypto_recv(be_connfd, &cipher_auth, sizeof(cipher_auth));
+  ret &= vcrypto_recv(client_fd, &cipher_auth, sizeof(cipher_auth));
   sess_resource *sr =  get_sess_resource(&cipher_auth); 
-  ret &= vcrypto_send(be_connfd, sr->sess, sizeof(sr->sess));
+  ret &= vcrypto_send(client_fd, sr->sess, sizeof(sr->sess));
   return ret;
 }
 
-bool vcrypto_be_protocol_remove_sess(int be_connfd) {
+bool vcrypto_be_protocol_remove_sess(int client_fd) {
   uint64_t hash_val;
-  bool ret = vcrypto_recv(be_connfd, &hash_val, sizeof(hash_val));
+  bool ret = vcrypto_recv(client_fd, &hash_val, sizeof(hash_val));
   release_sess_resource(hash_val);
   return ret;
 }
